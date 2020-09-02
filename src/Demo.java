6abda69d06d94f0987830ade1a4a675442628e0c
Doug Wozniak <1904uk@gmail.com>
2020-09-02 09:59:05 -0400

    go away

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("you suck");
-		
+		System.out.println("what is going on around here");
 	}
 
 }
